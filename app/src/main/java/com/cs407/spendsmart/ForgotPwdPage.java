99963df02fcd63c7116cc46a034e9bb75cff6381
waizin28 <waizinlinn.08@gmail.com>
2023-10-21 23:50:38 -0500

    added forgotPwd Activity

--- app/src/main/java/com/cs407/spendsmart/ForgotPwdPage.java
+++ app/src/main/java/com/cs407/spendsmart/ForgotPwdPage.java
@@ -2,9 +2,11 @@ package com.cs407.spendsmart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -13,6 +15,8 @@ public class ForgotPwdPage extends AppCompatActivity {
     private TextInputLayout textInputUsername, textInputEmail;
     private Button recover_btn;
 
+    private TextView redirectSignIn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +24,21 @@ public class ForgotPwdPage extends AppCompatActivity {
 
         textInputUsername = findViewById(R.id.forgot_userInput);
         textInputEmail = findViewById(R.id.forgot_emailInput);
-
+        redirectSignIn = findViewById(R.id.redirect_signIn);
 
         recover_btn = findViewById(R.id.recoverPwdBtn);
         recover_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 confirmInput(view);
+            }
+        });
+
+        redirectSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(intent);
             }
         });
     }
