c9986a22bef941fc12519981b11082a651126657
mvpillai-a <mvpillai@wisc.edu>
2023-10-21 22:48:23 -0500

    Updated app theme and icon, Added input error messages when sign up is pressed

--- app/src/main/java/com/cs407/spendsmart/SignUpPage.java
+++ app/src/main/java/com/cs407/spendsmart/SignUpPage.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignUpPage extends AppCompatActivity {
 
@@ -16,6 +18,70 @@ public class SignUpPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+    }
+
+    public void clickSignUp(View view) {
+        boolean formFilled = true;
+        EditText name = findViewById(R.id.nameInput);
+        EditText username = findViewById(R.id.usernameInput);
+        EditText email = findViewById(R.id.emailInput);
+        EditText password = findViewById(R.id.passwordInput);
+        EditText confirm = findViewById(R.id.confirmPassInput);
+
+        if(name.getText().toString().equals("") || !name.getText().toString().contains(" ")){
+            formFilled = false;
+            TextView errorTxt= findViewById(R.id.nameErrorTxt);
+            errorTxt.setText("Both First and Last Name Required");
+            errorTxt.setVisibility(View.VISIBLE);
+        } else{
+            findViewById(R.id.nameErrorTxt).setVisibility(View.INVISIBLE);
+        }
+        if(username.getText().toString().equals("")){
+            formFilled = false;
+            TextView errorTxt= findViewById(R.id.usernameErrorTxt);
+            errorTxt.setText("Username Required");
+            errorTxt.setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.usernameErrorTxt).setVisibility(View.INVISIBLE);
+        }
+        if(email.getText().toString().equals("")){
+            formFilled = false;
+            TextView errorTxt= findViewById(R.id.emailErrorTxt);
+            errorTxt.setText("Email Required");
+            errorTxt.setVisibility(View.VISIBLE);
+        }
+        else if(!email.getText().toString().contains("@")){
+            formFilled = false;
+            TextView errorTxt= findViewById(R.id.emailErrorTxt);
+            errorTxt.setText("Invalid Email");
+            errorTxt.setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.emailErrorTxt).setVisibility(View.INVISIBLE);
+        }
+        if(password.getText().toString().equals("")){
+            formFilled = false;
+            TextView errorTxt= findViewById(R.id.passwordErrorTxt);
+            errorTxt.setText("Password Required");
+            errorTxt.setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.passwordErrorTxt).setVisibility(View.INVISIBLE);
+        }
+        if(!password.getText().toString().equals(confirm.getText().toString())){
+            formFilled = false;
+            TextView errorTxt= findViewById(R.id.confirmErrorTxt);
+            errorTxt.setText("Passwords do not match");
+            errorTxt.setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.confirmErrorTxt).setVisibility(View.INVISIBLE);
+        }
+
+        if(formFilled){
+            enterUser();
+        }
+    }
+
+    public void enterUser() {
+        Toast.makeText(this, "Sign Up Success", Toast.LENGTH_SHORT).show();
     }
 
     // Password Visibility2:
