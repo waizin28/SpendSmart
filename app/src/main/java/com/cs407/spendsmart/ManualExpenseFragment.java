f39f1dbb9d870128d2db60185b097608995579f3
connor-kolb-13 <c.kolb13@outlook.com>
2023-11-09 15:21:23 -0600

    Manual Expense Backend Complete

--- app/src/main/java/com/cs407/spendsmart/ManualExpenseFragment.java
+++ app/src/main/java/com/cs407/spendsmart/ManualExpenseFragment.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,22 +15,50 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import android.widget.Button;
+import android.widget.Toast;
+
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ManualExpenseFragment extends Fragment {
 
     private Spinner spinnerInput;
     private EditText manualInputDate;
+    private EditText manualExpenseAmount;
     private DatePickerDialog.OnDateSetListener dateSetListener;
+    private FirebaseFirestore db;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_manual_expense, container, false);
+
+        // Initialize Firestore
+        db = FirebaseFirestore.getInstance();
+
+        // Back button functionality
+        ImageView backButton = view.findViewById(R.id.manualExpenseBackButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new instance of ExpenseFragment
+                ExpenseFragment expenseFragment = new ExpenseFragment();
+
+                // Perform the fragment replacement
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, expenseFragment); // Use the correct container ID
+                transaction.commit();
+            }
+        });
 
         // Date Picker
         manualInputDate = view.findViewById(R.id.manualInputDate);
@@ -91,6 +120,65 @@ public class ManualExpenseFragment extends Fragment {
             }
         });
 
+        // Setup the amount input
+        manualExpenseAmount = view.findViewById(R.id.manualExpenseAmount);
+
+        // Setup the confirm button
+        Button confirmButton = view.findViewById(R.id.manualExpenseConfirmButton);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addExpenseToDatabase();
+            }
+        });
+
         return view;
+    }
+    private void addExpenseToDatabase() {
+        String category = spinnerInput.getSelectedItem().toString();
+        String amountString = manualExpenseAmount.getText().toString();
+        String date = manualInputDate.getText().toString();
+        String email = getCurrentUserEmail(); // Implement this method to get the logged-in user's email
+
+        // Validate the input
+        if (category.isEmpty() || amountString.isEmpty() || date.isEmpty() || email.isEmpty()) {
+            Toast.makeText(getContext(), "Please fill in all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        double amount;
+        try {
+            amount = Double.parseDouble(amountString);
+        } catch (NumberFormatException e) {
+            Toast.makeText(getContext(), "Invalid amount", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Create a new transaction map
+        Map<String, Object> transaction = new HashMap<>();
+        transaction.put("category", category);
+        transaction.put("amount", amount);
+        transaction.put("date", date);
+        transaction.put("email", email);
+
+        // Add a new document with a generated ID
+        db.collection("transactions")
+                .add(transaction)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(getContext(), "Expense added successfully", Toast.LENGTH_SHORT).show();
+                    // Reset the input fields
+                    spinnerInput.setSelection(0);
+                    manualExpenseAmount.setText("");
+                    manualInputDate.setText("");
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(getContext(), "Error adding expense", Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    private String getCurrentUserEmail() {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+         return mAuth.getCurrentUser().getEmail();
+
     }
 }
